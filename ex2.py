cb83661f36d961420f850bb3c669aa0e1643f943
Joe <20455380@life.hkbu.edu.hk>
2021-02-04 23:03:28 +1000

    from A end

--- ex2.py
+++ ex2.py
@@ -1,5 +1,10 @@
-cars = ["Ford", "Volvo", "BMW"]
+# Write a function that prints all factors of the given parameter x
+def print_factor(x):
+  r = []
+  for i in range(1, x+1):
+    if x%(i) == 0:
+      r.append(i)
+  print(r)
 
-x = cars[0]
-
-print(x)
+if __name__ == '__main__':
+    print_factor(52633)
